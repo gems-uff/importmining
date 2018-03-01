7e8efcc6b2f0f7259d519c5786ed96e11ad327c6
marcelomuniz <mpjmuniz@github.com>
2018-03-01 12:52:56 -0300

    Added measurement of couplings

--- src/main/kotlin/br/uff/ic/collector/ExplicitImportCollector.kt
+++ src/main/kotlin/br/uff/ic/collector/ExplicitImportCollector.kt
@@ -1,24 +1,26 @@
 package br.uff.ic.collector
 
 import br.uff.ic.domain.Project
+import br.uff.ic.extensions.orNull
 import br.uff.ic.mining.DataSet
 import br.uff.ic.mining.Row
 import io.netty.util.internal.ConcurrentSet
+import kotlin.streams.toList
 
 
 class ExplicitImportCollector : ImportCollector {
 
     override fun collect(project : Project): DataSet {
 
-        val javaFiles = project.sourceFiles
-                /*sourcePaths
+        val javaFiles = project//.sourceFiles
+                .sourcePaths
          .parallelStream().map {
             orNull {
                 JavaFile.new(it)
             }
         }.filter {
             it != null
-        }.toList()*/
+        }.toList()
         /*val javaFiles = project.sourceFiles*/
 
         val projectPackages = javaFiles.map {
@@ -28,17 +30,19 @@ class ExplicitImportCollector : ImportCollector {
         }.toSet()
 
         val localImports = ConcurrentSet<String>()
-        javaFiles.parallelStream().map {
+        val srcFiles = javaFiles.parallelStream()
+                .map {
             val local = it!!.imports.filter { clazz ->
                 projectPackages.any {
                     clazz.contains(it)
                 }
             }.toSet()
             localImports.addAll(local)
-        }
+            it.copy(imports = local)
+        }.filter { it.imports.isNotEmpty() }.toList()
         val header = localImports.sorted()
-        val rows = javaFiles.map {
-            Row(it!!.getFilePath(), it.imports.toSet()  )
+        val rows = srcFiles.map {
+            Row(it!!.file.path, it.imports.toSet()  )
         }
         return DataSet(header, rows)
     }
---
+++ src/main/kotlin/br/uff/ic/domain/Coupling.kt
@@ -0,0 +1,6 @@
+package br.uff.ic.domain
+
+import br.uff.ic.mining.Rule
+
+class Coupling(val clazz : String, val rulesInvolved : Collection<Rule>) {
+}
